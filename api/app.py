748608f290273fcac46f3ba69296dad3a93d5ac2
kurianbenoy <kurian.bkk@gmail.com>
2021-07-05 00:28:50 +0530

    update webapi

--- api/app.py
+++ api/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect
 from fastai.vision.all import *
+from werkzeug.utils import secure_filename
+
+from .constants import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000 # specify upload file limit
+app.config['SECRET_KEY'] = 'secret!'	# for flash messages
 
-@app.route("/predict/tree")
+def allowed_files(filename):
+    """Reurn list of allowed urls"""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/predict/tree", methods=["POST", "GET"])
 def predict_tree():
-    learn_inf = load_learner("export.pkl")
-    # img = PILImage.create(upload_btn.data[-1])
-    pred, pred_idx, probs = learn_inf.predict(img)
-    return pred
+    """Predict the tree using Flask APIs"""
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_files(file.filename):
+            filename = secure_filename(file.filename)
+            learn_inf = load_learner("../export.pkl")
+            img = PILImage.create(filename)
+            pred_class, pred_idx, outputs = learn_inf.predict(img)
+            return pred_class
---
+++ api/constants.py
@@ -0,0 +1,2 @@
+UPLOAD_FOLDER = 'upload/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
